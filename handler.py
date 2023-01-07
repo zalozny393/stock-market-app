4348f596dd291605d2e979744a1b53ada8ea7bd6
Alex <zalozny393@gmail.com>
2023-01-06 21:43:10 -0500

    add yahoo finance

--- handler.py
+++ handler.py
@@ -1,4 +1,5 @@
 import os
+import yfinance as yf
 
 
 if os.environ.get("IS_OFFLINE") == "true" and os.environ.get("LOCAL_DEBUG") == "true":
@@ -8,5 +9,19 @@ if os.environ.get("IS_OFFLINE") == "true" and os.environ.get("LOCAL_DEBUG") == "
     debugpy.wait_for_client()
 
 
-def get_stock(event, context):
-    return {"symbol": event["arguments"]["symbol"]}
+def get_stock_info(event, context):
+    ticker = yf.Ticker(event["arguments"]["symbol"])
+
+    return {
+        "symbol": ticker.info["symbol"],
+        "currentPrice": ticker.info["currentPrice"],
+        "currency": ticker.info["financialCurrency"],
+        "beta": ticker.info["beta"],
+        "sector": ticker.info["sector"],
+        "longBusinessSummary": ticker.info["longBusinessSummary"],
+        "longName": ticker.info["longName"],
+        "forwardPE": ticker.info["forwardPE"],
+        "trailingPE": ticker.info["trailingPE"],
+        "fiveYearAvgDividendYield": ticker.info["fiveYearAvgDividendYield"],
+        "logo_url": ticker.info["logo_url"],
+    }
