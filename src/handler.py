bdce5f0c549fc2613cf042f808310af5b7c1f48c
Alex <zalozny393@gmail.com>
2023-01-09 22:31:54 -0500

    add multy ticker support

--- src/handler.py
+++ src/handler.py
@@ -10,18 +10,26 @@ if os.environ.get("IS_OFFLINE") == "true" and os.environ.get("LOCAL_DEBUG") == "
 
 
 def get_stock_info(event, context):
-    ticker = yf.Ticker(event["arguments"]["symbol"])
+    symbols = " ".join(event["arguments"]["symbols"])
+    tickers = yf.Tickers(symbols)
 
-    return {
-        "symbol": ticker.info["symbol"],
-        "currentPrice": ticker.info["currentPrice"],
-        "currency": ticker.info["financialCurrency"],
-        "beta": ticker.info["beta"],
-        "sector": ticker.info["sector"],
-        "longBusinessSummary": ticker.info["longBusinessSummary"],
-        "longName": ticker.info["longName"],
-        "forwardPE": ticker.info["forwardPE"],
-        "trailingPE": ticker.info["trailingPE"],
-        "fiveYearAvgDividendYield": ticker.info["fiveYearAvgDividendYield"],
-        "logo_url": ticker.info["logo_url"],
-    }
+    stocks_info = []
+    for symbol, ticker in tickers.tickers.items():
+        if not ticker.info:
+            continue
+        stocks_info.append(
+            {
+                "symbol": symbol,
+                "currentPrice": ticker.info["currentPrice"],
+                "currency": ticker.info["financialCurrency"],
+                "beta": ticker.info["beta"],
+                "sector": ticker.info["sector"],
+                "longBusinessSummary": ticker.info["longBusinessSummary"],
+                "longName": ticker.info["longName"],
+                "forwardPE": ticker.info["forwardPE"],
+                "trailingPE": ticker.info["trailingPE"],
+                "fiveYearAvgDividendYield": ticker.info["fiveYearAvgDividendYield"],
+                "logo_url": ticker.info["logo_url"],
+            }
+        )
+    return stocks_info
